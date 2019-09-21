9f51cfe88ca24c8b29b19348c1f2abdd14b7916b
jmarclan <jamarcolan@gmail.com>
2019-09-20 22:18:54 -0300

    Movimentando o player

--- app.js
+++ app.js
@@ -11,11 +11,136 @@ app.get('/', (req, res) => {
    });
 
 
+
 io.on('connection', (socket) => {
+    contr_player = new Cons_player(0)
+
+    socket.player = contr_player.get_player()
+
+    socket.on("mensagem", (msg)=>{
+        console.log(msg)
+
+        io.emit("mensagem", `O ${socket.id} enviou o mensagem: ${msg}`)
+    })
+
+
+    
+
+
+
+    socket.on("move_direita", ()=>{
+
+        socket.player.move_direita()
+
+        io.emit("atualiza_jogo", socket.player);
+        }
+    )
+
+    socket.on("move_esquerda", ()=>{
+
+        socket.player.move_esquerda()
+
+        io.emit("atualiza_jogo", socket.player);
+
+    }
+)
+
     console.log("oi mundo ta conectando o socket");
+
+
+
+
+
 })
 
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
   });
+
+class Cons_player{
+    constructor(n_player){
+        this.player = new Player()
+        let movimento = new Movimento(true)
+
+        this.player.set_movimento(movimento)
+
+
+    }
+
+    get_player(){
+        return this.player;
+    }
+}
+
+
+
+class Player{
+    constructor(){
+        this.vida = 0
+        this.pos ={
+            x : 10,
+            y : 10
+        }
+        
+    }
+
+    //arrumar como especifica o player
+    get_dados(){
+        return {x:this.pos.x
+               ,y:this.pos.y
+               ,heigh:140
+               ,width:150}
+    }
+
+    set_desenho(desenho){
+        this.desenho =desenho;
+    }
+
+    
+    set_movimento(movimento){
+         this.movimento =movimento;
+         this.movimento.set_data(this.pos)
+    }
+
+    move_direita(){
+        this.movimento.move_direita();
+
+    }
+    move_esquerda(){
+        this.movimento.move_esquerda();
+    }
+
+
+}
+
+class Movimento{
+    constructor(ch_horizontal){
+        this.ch_horizontal = ch_horizontal;
+        
+
+    }
+    set_data(pos){
+        this.pos = pos
+    }
+
+    move_direita(){
+        if(this.ch_horizontal){
+            this.pos.x = this.pos.x + 20
+
+        }else{
+            this.pos.y = this.pos.y + 20
+        }
+
+        
+    }
+    move_esquerda(){
+        if(this.ch_horizontal){
+            this.pos.x = this.pos.x -20
+        }else{
+
+            this.pos.y = this.pos.y -20
+        }
+
+    }
+}
--- assets/client-jogo.js
+++ assets/client-jogo.js
@@ -2,3 +2,18 @@ var socket = io();
 
 socket.emit("connect");
 
+
+socket.on("mensagem", (msg)=> console.log(msg))
+
+socket.on("atualiza_jogo", (msg)=> console.log(msg))
+
+function envia_msg(msg){
+    socket.emit("mensagem",msg);
+}
+
+function mov_direita(msg){
+    socket.emit("move_direita");
+}
+function mov_esquerda(msg){
+    socket.emit("move_esquerda");
+}
