88b25b7418a0fb5ac6714829af956c8f6b773a27
jmarclan <jamarcolan@gmail.com>
2019-09-21 00:03:12 -0300

    Funcionando as salas do game

--- app.js
+++ app.js
@@ -5,17 +5,74 @@ const io = require('socket.io')(http);
 
 app.use(express.static("./assets"));
 
+class Jogo{
+    constructor(sala){
+        this.sala = sala 
+        this.players = []
+
+        //envia que tem q matar o plyer
+    }
+
+    //funcao que faz tudo que é possivel HUASDHUASDHUHUASDUHASDHU
+    //necessário uma refatoração imediata :D
+    set_player(socket){
+
+        this.players.push(socket)
+
+        let contr_player = new Cons_player(socket.id)
+        socket.player = contr_player.get_player()
+
+        socket.join(this.sala);
+        io.to(this.sala).emit('comeca', "conectado");
+
+        socket.on("move_direita", ()=>{
+
+            socket.player.move_direita()
+    
+            io.to(this.sala).emit("atualiza_jogo", socket.player);
+            }
+        )
+    
+        socket.on("move_esquerda", ()=>{
+    
+            socket.player.move_esquerda()
+    
+            io.to(this.sala).emit("atualiza_jogo", socket.player);
+    
+        })
+
+        this.qnt_player = this.players.length
+
+
+    }
+
+
+}
+
+
+const jogos = [new Jogo(gera_nomes_aleatorios())]
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
    });
 
+//construir uma rota para pegar todos os jgoso ativos
+
+app.get('/pega_salas', (req, res) => {
+    let jogos_id = jogos.map((ele)=> {return ele.sala})
+    res.json({ jogos: jogos_id });
+   });
+
+app.get("conecta_room",(req, res)=>{
+
+    res.json({ sala: '1' })
+})
+
+
 
 
 io.on('connection', (socket) => {
-    contr_player = new Cons_player(0)
-
-    socket.player = contr_player.get_player()
+    
 
     socket.on("mensagem", (msg)=>{
         console.log(msg)
@@ -23,35 +80,80 @@ io.on('connection', (socket) => {
         io.emit("mensagem", `O ${socket.id} enviou o mensagem: ${msg}`)
     })
 
+ 
+    socket.on("conecta_sala",(sala)=>{
+        conecta_sala(sala, socket)
+        
+    })
 
-    
-
-
-
-    socket.on("move_direita", ()=>{
-
-        socket.player.move_direita()
-
-        io.emit("atualiza_jogo", socket.player);
-        }
-    )
-
-    socket.on("move_esquerda", ()=>{
-
-        socket.player.move_esquerda()
-
-        io.emit("atualiza_jogo", socket.player);
-
-    }
-)
 
     console.log("oi mundo ta conectando o socket");
 
 
-
-
-
 })
+
+
+ 
+
+function gera_nomes_aleatorios(){
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+//precisa remover of ato de coenctar com o de criar novas salas
+
+function conecta_sala(sala,socket){
+
+    sala_enc = get_sala_by_id(sala)
+
+    sala_enc.set_player(socket)
+
+
+}
+
+
+function get_sala_by_id(sala){
+    let sala_jogo_encontrada = null 
+    jogos.forEach(ele =>{
+        if(ele.sala == sala )
+        sala_jogo_encontrada = ele
+
+    })
+
+    return sala_jogo_encontrada;
+}
+
+function create_jogo(sala,socket){
+
+    jogos.forEach(ele =>{
+        let regra_sala_Existe = ele.sala === sala
+        if(regra_sala_Existe ){
+            let regra_Sala_cheia = ele.qnt_player === 4
+            if(regra_Sala_cheia){
+
+                let jogo =new Jogo(gera_nomes_aleatorios())
+                jogo.set_player(socket)
+                jogos.append(jogo)
+            }else{
+                ele.set_player(socket)
+            }
+        
+
+        }else{
+            let jogo =new Jogo(gera_nomes_aleatorios())
+            jogo.set_player(socket)
+            jogos.append( jogo)
+        }
+        
+
+    })
+
+
+
+
+}
+
+
+
 
 
 http.listen(3000, () => {
@@ -59,8 +161,8 @@ http.listen(3000, () => {
   });
 
 class Cons_player{
-    constructor(n_player){
-        this.player = new Player()
+    constructor(id_player){
+        this.player = new Player(id_player)
         let movimento = new Movimento(true)
 
         this.player.set_movimento(movimento)
@@ -76,16 +178,19 @@ class Cons_player{
 
 
 class Player{
-    constructor(){
+    constructor(id_player){
         this.vida = 0
         this.pos ={
             x : 10,
             y : 10
         }
+
+        this.id = id_player
         
     }
 
     //arrumar como especifica o player
+    //defasado nems ei se funciona mais HUShudsauhhuSADHUASDHUhuds
     get_dados(){
         return {x:this.pos.x
                ,y:this.pos.y
@@ -124,6 +229,7 @@ class Movimento{
         this.pos = pos
     }
 
+    //conferir se a logica esta fucnionando
     move_direita(){
         if(this.ch_horizontal){
             this.pos.x = this.pos.x + 20
--- assets/client-jogo.js
+++ assets/client-jogo.js
@@ -3,9 +3,84 @@ var socket = io();
 socket.emit("connect");
 
 
+//estou plorando coisas queu nao deveria estar explorando
+//HUADSHUASDHUASDHUAHUSDhuASD
+//mas isso nao é uma live didatica SHAUDHUASDHUADSHUAHUDShuads
+//refatore essa gambiarra XD
+$.getJSON('/pega_salas', 
+function (data, textStatus, jqXHR){
+    this.data = data 
+    data.jogos.forEach(ele => {
+    let buto = $(`<button>${ele}</button>`)
+    let envia = () =>{
+        console.log(ele)
+        socket.emit("conecta_sala",ele);
+    
+    }
+    buto.on("click",() => envia())
+
+    $('#sala').append(buto);
+    })
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Game{
+
+    constructor(){
+
+        this.conj_ids = new Set()
+    
+        this.get_dados =(id)=>{
+            
+            this.conj_ids.add(id)
+            this.mostra_id()
+        }
+
+        socket.on("set_ids", this.get_dados)
+        socket.emit("get_ids");
+
+    }
+
+    mostra_id(){
+
+        this.conj_ids.forEach(ele => console.log(ele))
+
+    }
+
+    get_player_by_id(id){
+        player = null;
+        jogadores.forEach(ele =>{
+          if (ele.id == id) {
+            player = ele;
+          }
+        })
+        return player;
+      }
+
+}
+
+const game = new Game()
+
+
 socket.on("mensagem", (msg)=> console.log(msg))
 
-socket.on("atualiza_jogo", (msg)=> console.log(msg))
+
+socket.on("atualiza_jogo", (msg)=> {
+    console.log(msg)
+    })
 
 function envia_msg(msg){
     socket.emit("mensagem",msg);
@@ -17,3 +92,5 @@ function mov_direita(msg){
 function mov_esquerda(msg){
     socket.emit("move_esquerda");
 }
+
+
