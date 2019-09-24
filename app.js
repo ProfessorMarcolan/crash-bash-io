8ea6b1643948f00851eecb06aa01735e82456c23
jmarcolan <jamarcolan@gmail.com>
2019-09-23 23:22:33 -0300

    Fizemos um monte de cisa qu enao lembramos HUSDHUDHUASHUASDHUhusad pra que mintir

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.static("./assets"));
 
 
 
+
 class Sala{
     constructor(sala){
         this.sala = sala 
@@ -14,38 +15,95 @@ class Sala{
         //talvez nos se arrependeremos dessa decição :D
         this.jogo = new Jogo(this.sala, this.players)
 
-    }
+        
 
-    set_player(socket){
-
-        this.players.push(socket)
-
-        let contr_player = new Cons_player(socket,this.sala)
-        socket.player = contr_player.get_player()
-
-        socket.join(this.sala);
-        //io.to(this.sala).emit('comeca', "conectado");
 
         
 
-        socket.on("queu", ()=>{
-    
+        //this.estados = [jogando, pausado, iniciando]
+
+    }
+
+    queu(){
     
             io.to(this.sala).emit("queu", 
-            this.players.map((ele)=>{return ele.id}));
+            //provavelmente tem que arrumar para ter estado
+            this.players.map((ele)=>{return { id: ele.id, stado:false}}));
     
-        })
+        
+    }
+
+
+    set_socket_sala(socket){
+
+        let regra_max_qtd_player= this.players.length <= 4 
+
+        if(regra_max_qtd_player){
+            this.players.push(socket)
+
+            //gambiarra vai dar problema no futuro
+            //levar essa responsabilidade para a classe jogo
+            let contr_player = new Cons_player(socket, this.sala, this.jogo)
+            this.players.push(contr_player.get_player())
+            socket.on("comeca_jogo",()=> this.jogo.comeca_jogo() )
+            socket.on("queu", ()=> this.queu)
+            //socket.player = contr_player.get_player()
+
+
+            socket.join(this.sala);
+            this.queu()
+            this.qnt_player = this.players.length
+
+        }else{
+            this.starta_jogo()
+
+        }
+        
+    }
+
+    starta_jogo(){
+
+        io.to(this.sala).emit("queu", 
+        //provavelmente tem que arrumar para ter estado
+        this.players.map((ele)=>{return { id: ele.id, stado:true}}));
+
+        this.jogo.starta_jogo()
+
+    }
 
 
 
+    //precisa ser refatorado
+    //seta conexão
+    set_player(socket){
+
+        this.players.push(socket)
+        socket.join(this.sala);
+        this.queu()
 
         this.qnt_player = this.players.length
 
 
     }
+
+
 }
 
+class Cons_player{
+    constructor(socket, sala, jogo){
+        this.sala = sala
+        this.player = new Player(socket,sala, jogo)
+        let movimento = new Movimento(true)
 
+        this.player.set_movimento(movimento)
+
+    }
+
+
+    get_player(){
+        return this.player;
+    }
+}
 
 
 
@@ -54,13 +112,48 @@ class Jogo{
         this.sala = sala 
         this.players = []
         this.players = players
+
+        
+        
+        //this.socket.on("comeca_jogo",()=> this.comeca_jogo() )
     }
 
+    //inicio, fim ,pausa (nao se pausa jogo online HUASDHUADSHU)
+    
+    comeca_jogo(){
 
+        io.to(this.sala).emit("comeca_jogo", true);
+        
+    }
 
+    //toda vez que o player se mover
+    regras_jogo(){
+        //player morreu
+        //player levo um gol
+        //posicao da bola 
 
+        this.envia_estado_jogo()
+    }
+
+    get_dados(){
+        //let player_pos = this.players.map((ele)=> {ele.get_dados()})
+
+        return [{players:[], estado:true}]
+
+    }
+
+    envia_estado_jogo(){
+
+        io.to(this.sala).emit("atualiza_jogo", this.get_dados());
+        
+    }
 
 }
+function gera_nomes_aleatorios(){
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+
 
 const salas = [new Sala(gera_nomes_aleatorios())]
 
@@ -75,13 +168,10 @@ app.get('/pega_salas', (req, res) => {
     res.json({ jogos: jogos_id });
    });
 
-app.get("conecta_room",(req, res)=>{
-
-    res.json({ sala: '1' })
-})
 
 
 
+//estart poitn do nosso projeto
 
 io.on('connection', (socket) => {
     
@@ -93,10 +183,13 @@ io.on('connection', (socket) => {
     })
 
  
+    //refatorar
     socket.on("conecta_sala",(sala)=>{
         conecta_sala(sala, socket)
         
     })
+    
+    
 
 
     console.log("oi mundo ta conectando o socket");
@@ -104,20 +197,12 @@ io.on('connection', (socket) => {
 
 })
 
-
- 
-
-function gera_nomes_aleatorios(){
-    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-}
-
-//precisa remover of ato de coenctar com o de criar novas salas
-
+//isso precisa ser refatorado
 function conecta_sala(sala,socket){
 
     sala_enc = get_sala_by_id(sala)
 
-    sala_enc.set_player(socket)
+    sala_enc.set_socket_sala(socket)
 
 
 }
@@ -135,6 +220,15 @@ function get_sala_by_id(sala){
 }
 
 
+
+ 
+
+
+//precisa remover of ato de coenctar com o de criar novas salas
+
+
+
+/*
 //funcao depreceada temq ue arruma para apenas crear jogos
 function create_jogo(sala,socket){
 
@@ -165,7 +259,7 @@ function create_jogo(sala,socket){
 
 
 }
-
+*/
 
 
 
@@ -174,25 +268,13 @@ http.listen(3000, () => {
     console.log('listening on *:3000');
   });
 
-class Cons_player{
-    constructor(socket, sala){
-        this.player = new Player(socket,sala)
-        let movimento = new Movimento(true)
 
-        this.player.set_movimento(movimento)
-
-
-    }
-
-    get_player(){
-        return this.player;
-    }
-}
 
 
 
 class Player{
-    constructor(socket_player,sala){
+    constructor(socket_player,sala, jogo){
+        this.jogo = jogo
         this.sala = sala 
         this.vida = 0
         this.pos ={
@@ -233,19 +315,22 @@ class Player{
 
     move_direita(){
         this.movimento.move_direita();
+        this.jogo.regras_jogo()
 
         //arrumar o que envia
-        io.to(this.sala).emit("atualiza_jogo", this.get_dados());
+       
 
     }
     move_esquerda(){
         this.movimento.move_esquerda();
+        this.jogo.regras_jogo()
 
-        //arrumar o que envia
-        io.to(this.sala).emit("atualiza_jogo", this.get_dados());
+
+        
     }
 
     atira_poder(){
+        this.jogo.regras_jogo()
 
     }
 
